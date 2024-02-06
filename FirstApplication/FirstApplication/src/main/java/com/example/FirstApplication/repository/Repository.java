01692f197d5f90980076d4571ca901b6523ab538
fidyanhakim-technman <155768580+fidyanhakim-technman@users.noreply.github.com>
2024-02-05 19:28:23 -0500

    Delete FirstApplication directory

--- FirstApplication/FirstApplication/src/main/java/com/example/FirstApplication/repository/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.FirstApplication.repository;
-
-import com.example.FirstApplication.model.Consultant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@org.springframework.stereotype.Repository
-public interface Repository extends JpaRepository<Consultant,Long> {
-
-}
